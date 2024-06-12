4f567de5518197abe00d0e816e8f4df39f1fb81f
hilalahmad0101 <stackf63@gmail.com>
2024-06-12 11:12:02 +0200

    add popup

--- resources/views/layouts/new_app.blade.php
+++ resources/views/layouts/new_app.blade.php
@@ -23,8 +23,8 @@
             </svg>
         </div>
         <div id="show_data" class="absolute hidden top-24 left-0 w-full bg-[#2e2e2e]">
-            <ul class=" px-3">
-                <li class="pt-[20px]  px-4 pb-10">
+            <ul class=" px-3  pb-10">
+                <li class="pt-[20px]  px-4">
                     <a href="{{ route('admin.dashboard') }}" class="flex items-center space-x-4">
                         <img src="{{ asset('images/home.png') }}" class="" alt="">
                         <span class="text-center text-neutral-500 text-xs font-normal ">Home</span>
@@ -32,7 +32,7 @@
                 </li>
                 <li class="pt-[20px] px-4">
                     <a class="flex items-center space-x-4" href="{{ route('admin.admin.index') }}">
-                        <img src="{{ Request::routeIs('admin.admin.index') ? asset('icons/user-yellow.svg') : asset('icons/user-white.svg') }}"
+                        <img src="{{ Request::routeIs('admin.admin.index') ? asset('icons/user-yellow.png') : asset('icons/user-white.png') }}"
                             class="" alt="">
                         <span
                             class="text-center  text-xs font-normal {{ Request::routeIs('admin.admin.index') ? 'text-[#FFA800]' : 'text-neutral-500' }} ">Admin</span>
@@ -40,7 +40,7 @@
                 </li>
                 <li class="pt-[20px] px-4">
                     <a class="flex items-center space-x-4" href="{{ route('admin.user.index') }}">
-                        <img src="{{ Request::routeIs('admin.user.index') ? asset('icons/users-yellow.svg') : asset('icons/users-white.svg') }}"
+                        <img src="{{ Request::routeIs('admin.user.index') ? asset('icons/users-yellow.png') : asset('icons/users-white.svg') }}"
                             class="" alt="">
                         <span
                             class="text-center {{ Request::routeIs('admin.user.index') ? 'text-[#FFA800]' : 'text-neutral-500' }}  text-xs font-normal ">User</span>
@@ -58,7 +58,7 @@
 
                 <li class="pt-[20px] px-4">
                     <a class="flex items-center space-x-4" href="{{ route('admin.actor.index') }}">
-                        <img src="{{ Request::routeIs('admin.actor.index') ? asset('icons/cost-yellow.svg') : asset('icons/cost-white.svg') }}"
+                        <img src="{{ Request::routeIs('admin.actor.index') ? asset('icons/cast-yellow.png') : asset('icons/cost-white.svg') }}"
                             class="" alt="">
                         <span
                             class="text-center {{ Request::routeIs('admin.actor.index') ? 'text-[#FFA800]' : 'text-neutral-500' }}  text-xs font-normal ">Cast</span>
@@ -76,7 +76,7 @@
 
                 <li class="pt-[20px] px-4">
                     <a class="flex items-center space-x-4" href="{{ route('admin.review.index') }}">
-                        <img src="{{ Request::routeIs('admin.review.index') ? asset('icons/bell-yellow.svg') : asset('icons/bell-white.svg') }}"
+                        <img src="{{ Request::routeIs('admin.review.index') ? asset('icons/review-yellow.png') : asset('icons/review-white.svg') }}"
                             class="" alt="">
                         <span
                             class="text-center {{ Request::routeIs('admin.review.index') ? 'text-[#FFA800]' : 'text-neutral-500' }}  text-xs font-normal ">Review</span>
@@ -84,7 +84,7 @@
                 </li>
                 <li class="pt-[20px] px-4">
                     <a class="flex items-center space-x-4" href="{{ route('admin.notification.index') }}">
-                        <img src="{{ Request::routeIs('admin.notification.index') ? asset('icons/bell-yellow.svg') : asset('icons/bell-white.svg') }}"
+                        <img src="{{ Request::routeIs('admin.notification.index') ? asset('icons/notification-yellow.png') : asset('icons/bell-white.svg') }}"
                             class="" alt="">
                         <span
                             class="text-center {{ Request::routeIs('admin.notification.index') ? 'text-[#FFA800]' : 'text-neutral-500' }}  text-xs font-normal ">Notification</span>
@@ -92,7 +92,7 @@
                 </li>
                 <li class="pt-[20px] px-4">
                     <a class="flex items-center space-x-4" href="{{ route('admin.legal.index') }}">
-                        <img src="{{ Request::routeIs('admin.legal.index') ? asset('icons/legal-yellow.svg') : asset('icons/legal-white.svg') }}"
+                        <img src="{{ Request::routeIs('admin.legal.index') ? asset('icons/legal-yellow.png') : asset('icons/legal-white.png') }}"
                             class="" alt="">
                         <span
                             class="text-center {{ Request::routeIs('admin.legal.index') ? 'text-[#FFA800]' : 'text-neutral-500' }}  text-xs font-normal ">Legal</span>
@@ -108,7 +108,7 @@
 
                 <li class="pt-[20px] px-4">
                     <a class="flex items-center space-x-4" href="{{ route('admin.jingle.index') }}">
-                        <img src="{{ Request::routeIs('admin.jingle.index') ? asset('icons/ads-yellow.svg') : asset('icons/ads-white.svg') }}"
+                        <img src="{{ Request::routeIs('admin.jingle.index') ? asset('icons/ads-yellow.png') : asset('icons/ads-white.svg') }}"
                             class="" alt="">
                         <span
                             class="text-center {{ Request::routeIs('admin.admin.index') ? 'text-[#FFA800]' : 'text-neutral-500' }}  text-xs font-normal ">Advertising</span>
@@ -139,7 +139,7 @@
                     </li>
                     <li class="pt-[50px]">
                         <a href="{{ route('admin.admin.index') }}">
-                            <img src="{{ Request::routeIs('admin.admin.index') ? asset('icons/user-yellow.svg') : asset('icons/user-white.svg') }}"
+                            <img src="{{ Request::routeIs('admin.admin.index') ? asset('icons/user-yellow.png') : asset('icons/user-white.png') }}"
                                 class="mx-auto" alt="">
                             <span
                                 class="text-center  text-xs font-normal {{ Request::routeIs('admin.admin.index') ? 'text-[#FFA800]' : 'text-neutral-500' }} ">Admin</span>
@@ -147,7 +147,7 @@
                     </li>
                     <li class="pt-[50px]">
                         <a href="{{ route('admin.user.index') }}">
-                            <img src="{{ Request::routeIs('admin.user.index') ? asset('icons/users-yellow.svg') : asset('icons/users-white.svg') }}"
+                            <img src="{{ Request::routeIs('admin.user.index') ? asset('icons/users-yellow.png') : asset('icons/users-white.svg') }}"
                                 class="mx-auto" alt="">
                             <span
                                 class="text-center {{ Request::routeIs('admin.user.index') ? 'text-[#FFA800]' : 'text-neutral-500' }}  text-xs font-normal ">User</span>
@@ -165,7 +165,7 @@
 
                     <li class="pt-[50px]">
                         <a href="{{ route('admin.actor.index') }}">
-                            <img src="{{ Request::routeIs('admin.actor.index') ? asset('icons/cost-yellow.svg') : asset('icons/cost-white.svg') }}"
+                            <img src="{{ Request::routeIs('admin.actor.index') ? asset('icons/cast-yellow.png') : asset('icons/cost-white.svg') }}"
                                 class="mx-auto" alt="">
                             <span
                                 class="text-center {{ Request::routeIs('admin.actor.index') ? 'text-[#FFA800]' : 'text-neutral-500' }}  text-xs font-normal ">Cast</span>
@@ -183,7 +183,7 @@
 
                     <li class="pt-[50px]">
                         <a href="{{ route('admin.review.index') }}">
-                            <img src="{{ Request::routeIs('admin.review.index') ? asset('icons/bell-yellow.svg') : asset('icons/bell-white.svg') }}"
+                            <img src="{{ Request::routeIs('admin.review.index') ? asset('icons/review-yellow.png') : asset('icons/review-white.svg') }}"
                                 class="mx-auto" alt="">
                             <span
                                 class="text-center {{ Request::routeIs('admin.review.index') ? 'text-[#FFA800]' : 'text-neutral-500' }}  text-xs font-normal ">Review</span>
@@ -191,7 +191,7 @@
                     </li>
                     <li class="pt-[50px]">
                         <a href="{{ route('admin.notification.index') }}">
-                            <img src="{{ Request::routeIs('admin.notification.index') ? asset('icons/bell-yellow.svg') : asset('icons/bell-white.svg') }}"
+                            <img src="{{ Request::routeIs('admin.notification.index') ? asset('icons/notification-yellow.png') : asset('icons/bell-white.svg') }}"
                                 class="mx-auto" alt="">
                             <span
                                 class="text-center {{ Request::routeIs('admin.notification.index') ? 'text-[#FFA800]' : 'text-neutral-500' }}  text-xs font-normal ">Notification</span>
@@ -199,7 +199,7 @@
                     </li>
                     <li class="pt-[50px]">
                         <a href="{{ route('admin.legal.index') }}">
-                            <img src="{{ Request::routeIs('admin.legal.index') ? asset('icons/legal-yellow.svg') : asset('icons/legal-white.svg') }}"
+                            <img src="{{ Request::routeIs('admin.legal.index') ? asset('icons/legal-yellow.png') : asset('icons/legal-white.png') }}"
                                 class="mx-auto" alt="">
                             <span
                                 class="text-center {{ Request::routeIs('admin.legal.index') ? 'text-[#FFA800]' : 'text-neutral-500' }}  text-xs font-normal ">Legal</span>
@@ -215,23 +215,24 @@
 
                     <li class="pt-[50px]">
                         <a href="{{ route('admin.jingle.index') }}">
-                            <img src="{{ Request::routeIs('admin.jingle.index') ? asset('icons/ads-yellow.svg') : asset('icons/ads-white.svg') }}"
+                            <img src="{{ Request::routeIs('admin.jingle.index') ? asset('icons/ads-yellow.png') : asset('icons/ads-white.svg') }}"
                                 class="mx-auto" alt="">
                             <span
-                                class="text-center {{ Request::routeIs('admin.admin.index') ? 'text-[#FFA800]' : 'text-neutral-500' }}  text-xs font-normal ">Advertising</span>
+                                class="text-center {{ Request::routeIs('admin.jingle.index') ? 'text-[#FFA800]' : 'text-neutral-500' }}  text-xs font-normal ">Advertising</span>
                         </a>
                     </li>
 
                     <li class="pt-[50px]">
                         <a href="{{ route('admin.logout') }}">
                             <img src="{{ asset('images/logout.png') }}" class="mx-auto" alt="">
-                            <span
-                                class="text-center {{ Request::routeIs('admin.jingle.index') ? 'text-[#FFA800]' : 'text-neutral-500' }}  text-xs font-normal ">Logout</span>
+                            <span class="text-center text-neutral-500 text-xs font-normal ">Logout</span>
                         </a>
                     </li>
                 </ul>
             </nav>
         </div>
+
+
 
         <div class="w-full">
             <main class="container mx-auto w-full lg:px-[60px] px-3">
@@ -338,21 +339,53 @@
     </script>
 
     @if (session()->has('success'))
-        <script>
-            toastr['success']("{{ session('success') }}")
-        </script>
+        <div class="fixed top-0 right-0 left-0" id="close_model">
+            <div class="flex items-center justify-center h-screen w-full bg-gray-900">
+                <div class="bg-gray-800 text-center container mx-auto py-20 p-8  rounded-md max-w-2xl shadow-lg">
+                    <div class="flex justify-center items-center mb-4">
+                        <div class="w-16 h-16 bg-green-500 rounded-full flex items-center justify-center">
+                            <svg class="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M5 13l4 4L19 7"></path>
+                            </svg>
+                        </div>
+                    </div>
+                    <h2 class="text-white text-2xl mb-4">{{ session('success') }}</h2>
+                    <button onclick="document.getElementById('close_model').classList.add('hidden')"
+                        class="bg-white text-gray-800 px-4 py-2 rounded-full hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-600">
+                        Close this popup
+                    </button>
+                </div>
+            </div>
+        </div>
     @elseif(session()->has('error'))
-        <script>
-            toastr['error']("{{ session('error') }}")
-        </script>
+        <div class="fixed top-0 right-0 left-0" id="close_model">
+            <div class="flex items-center justify-center h-screen w-full bg-gray-900">
+                <div class="bg-gray-800 text-center container mx-auto py-20 p-8  rounded-md max-w-2xl shadow-lg">
+                    <div class="flex justify-center items-center mb-4">
+                        <div class="w-16 h-16 bg-green-500 rounded-full flex items-center justify-center">
+                            <svg class="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M5 13l4 4L19 7"></path>
+                            </svg>
+                        </div>
+                    </div>
+                    <h2 class="text-white text-2xl mb-4">{{ session('error') }}</h2>
+                    <button onclick="document.getElementById('close_model').classList.add('hidden')"
+                        class="bg-white text-gray-800 px-4 py-2 rounded-full hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-600">
+                        Close this popup
+                    </button>
+                </div>
+            </div>
+        </div>
     @endif
 
     <script>
         $("#click_button").on('click', function() {
             $("#show_data").toggleClass('block hidden');
         });
-
-       
     </script>
 
     @yield('script')
